144b5d4b191fc86c30506bc87847d6ebda075cdd
joaolfp <joaolucasfp2001@gmail.com>
2024-05-07 23:46:42 -0300

    Updated layout and setup validation to workspace

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::{Command, Stdio};
+use std::process::{exit, Command, Stdio};
 use std::io::{BufReader, BufRead};
 use std::time::Instant;
 use std::env;
@@ -16,19 +16,30 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 4 {
+    if args.len() > 5 {
         let arg1 = &args[1];
         let arg2 = &args[2];
         let arg3 = &args[3];
         let arg4 = &args[4];
+        let arg5 = &args[5];
+
+        let get_arg1 = match arg1.as_str() {
+            "project" => "project",
+            "workspace" => "workspace",
+            _ => {
+                let text_error_arguments = "Error in arguments".red();
+            println!("{}", text_error_arguments);
+            exit(1)
+            }
+        };
 
         let output = Command::new("sh")
         .arg("-c")
         .arg(format!(
-            "set -o pipefail && xcodebuild -project {} \
+            "set -o pipefail && xcodebuild -{} {} \
              -scheme {} \
              -destination platform=iOS\\ Simulator,OS={},name=iPhone\\ {} \
-             clean test | xcpretty", arg1, arg2, arg3, arg4))
+             clean test | xcpretty", get_arg1, arg2, arg3, arg4, arg5))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
@@ -59,7 +70,9 @@ fn main() {
         results(start_time, passed_tests, failed_tests);
         validation_show_errors(get_errors);
     } else {
-        println!("Error in arguments");
+        let text_error_arguments = "Error in arguments".red();
+        println!("{}", text_error_arguments);
+        exit(1)
     }
 }
 
@@ -74,10 +87,12 @@ fn validation_lines(
         if let Some(start) = line.find("Test Suite") {
             if let Some(end) = line.find("started") {
                 let mut test_name = line[start+11..end].trim().to_string();
+
                 if test_name.ends_with(".xctest") {
                     test_name.truncate(test_name.len() - 7);
                 }
-                println!("{}", test_name);
+
+                println!("\n{}", test_name.purple());
                 file_names.push(test_name);
             }
         }
@@ -86,36 +101,44 @@ fn validation_lines(
     if line.contains("✓") {
         *line = line.replace("✓", "✅");
         *passed_tests += 1;
+
         println!("{}", line.green());
     } else if line.contains("✗") {
         *line = line.replace("✗", "❌");
         *failed_tests += 1;
+
         println!("{}", line.red());
         get_errors.push(line.clone());
     }
 }
 
 fn results(start_time: Instant, passed_tests: i32, failed_tests: i32) {
-    println!("\n🗳️  The results have been completed below\n");
 
-    let duration = start_time.elapsed();
+    if passed_tests == 0 && failed_tests == 0 {
+        let text_error_command = "We had a problem with the command, I recommend you check if you are using the correct arguments".red();
+        println!("{}", text_error_command);
+        exit(1);
+    } else {
+        println!("\n\n🗳️  The results have been completed below\n");
 
-    let mut table = Table::new();
+        let duration = start_time.elapsed();
 
-    let titles = vec!["Runtime", "Total Tests", "✅ Passed Tests", " ❌ Failed Tests"];
-    table.set_header(titles);
+        let mut table = Table::new();
 
-    let formatted_duration = format!("{:.2?}", duration);
-    let total_tests = passed_tests + failed_tests;
-    let row = vec![
-        formatted_duration.to_string(), 
-        total_tests.to_string(), 
-        passed_tests.to_string(),
-        failed_tests.to_string()];
+        let titles = vec!["Runtime", "Total Tests", "✅ Passed Tests", " ❌ Failed Tests"];
+        table.set_header(titles);
 
-    table.add_row(row);
+        let formatted_duration = format!("{:.2?}", duration);
+        let total_tests = passed_tests + failed_tests;
+        let row = vec![
+            formatted_duration.to_string(), 
+            total_tests.to_string(), 
+            passed_tests.to_string(),
+            failed_tests.to_string()];
 
-    println!("{table}");
+        table.add_row(row);
+        println!("{table}");
+    }
 }
 
 fn validation_show_errors(get_errors: Vec<String>) {
