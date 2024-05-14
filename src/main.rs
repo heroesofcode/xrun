03f5a1de2dee89f95e55264f3cb3c36d293af4a8
joaolfp <joaolucasfp2001@gmail.com>
2024-05-14 17:01:44 -0300

    Separate logic another files

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use std::process::{exit, Command, Stdio};
 use std::io::{BufReader, BufRead};
 use std::time::Instant;
 use std::env;
-use comfy_table::Table;
 use colored::Colorize;
 
 mod header;
-mod validation;
+mod validation_lines;
+mod results;
 
 use crate::header::*;
-use crate::validation::*;
+use crate::validation_lines::*;
+use crate::results::*;
 
 fn main() {
     header();
@@ -74,53 +75,5 @@ fn main() {
     } else {
         println!("{}", "Error in arguments".red());
         exit(1)
-    }
-}
-
-fn results(start_time: Instant, passed_tests: i32, failed_tests: i32) {
-
-    if passed_tests == 0 && failed_tests == 0 {
-        println!("{}", "We had a problem with the command, I recommend you check if you are using the correct arguments".red());
-        exit(1);
-    } else {
-        println!("\n\n🗳️  The results have been completed below\n");
-
-        let duration = start_time.elapsed();
-
-        let mut table = Table::new();
-
-        let titles = vec!["Runtime", "Total Tests", "✅ Passed Tests", " ❌ Failed Tests"];
-        table.set_header(titles);
-
-        let formatted_duration = format!("{:.2?}", duration);
-        let total_tests = passed_tests + failed_tests;
-        let row = vec![
-            formatted_duration.to_string(), 
-            total_tests.to_string(), 
-            passed_tests.to_string(),
-            failed_tests.to_string()];
-
-        table.add_row(row);
-        println!("{table}");
-    }
-}
-
-fn validation_show_errors(get_errors: Vec<String>) {
-    let mut table = Table::new();
-
-    let titles = vec!["Errors found"];
-    table.set_header(titles);
-
-    if get_errors.is_empty() {
-        println!("{}", "\n👏 Congratulations, no errors were found!!!".green())
-    } else {
-        println!("{}", "\n ⚠️  Below contains the errors \n".yellow());
-        
-        for errors in &get_errors {
-            let row = vec![errors.to_string()];
-            table.add_row(row);
-        }
-
-        println!("{table}");
     }
 }
---
+++ src/results.rs
@@ -0,0 +1,52 @@
+use std::process::exit;
+use std::time::Instant;
+use comfy_table::Table;
+use colored::Colorize;
+
+pub fn results(start_time: Instant, passed_tests: i32, failed_tests: i32) {
+
+    if passed_tests == 0 && failed_tests == 0 {
+        println!("{}", "We had a problem with the command, I recommend you check if you are using the correct arguments".red());
+        exit(1);
+    } else {
+        println!("\n\n🗳️  The results have been completed below\n");
+
+        let duration = start_time.elapsed();
+
+        let mut table = Table::new();
+
+        let titles = vec!["Runtime", "Total Tests", "✅ Passed Tests", " ❌ Failed Tests"];
+        table.set_header(titles);
+
+        let formatted_duration = format!("{:.2?}", duration);
+        let total_tests = passed_tests + failed_tests;
+        let row = vec![
+            formatted_duration.to_string(), 
+            total_tests.to_string(), 
+            passed_tests.to_string(),
+            failed_tests.to_string()];
+
+        table.add_row(row);
+        println!("{table}");
+    }
+}
+
+pub fn validation_show_errors(get_errors: Vec<String>) {
+    let mut table = Table::new();
+
+    let titles = vec!["Errors found"];
+    table.set_header(titles);
+
+    if get_errors.is_empty() {
+        println!("{}", "\n👏 Congratulations, no errors were found!!!".green())
+    } else {
+        println!("{}", "\n ⚠️  Below contains the errors \n".yellow());
+        
+        for errors in &get_errors {
+            let row = vec![errors.to_string()];
+            table.add_row(row);
+        }
+
+        println!("{table}");
+    }
+}
