501c711d67142ea91d2ffc5e26cd3ad1fef74223
joaolfp <joaolucasfp2001@gmail.com>
2025-05-01 01:29:56 -0300

    refactor: add sample iOS project

---
+++ sample/sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample
+//
+//  Created by João Lucas on 01/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
