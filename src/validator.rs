99c26d5b0622d52982a6244265cd12985e018d85
joaolfp <joaolucasfp2001@gmail.com>
2025-11-25 15:03:22 -0300

    fix: fix commented

--- src/validator.rs
+++ src/validator.rs
@@ -46,11 +46,11 @@ impl Validator {
                 Err("Tests failed (file generated)".into())
             },
             (ValidationArg::GenerateFile, ValidationArg::Fail) => {
-                Err("Argument error: conflict between fail and generate-file".red().to_string())
+                Err("Argument error: conflict between fail and generate-file".into())
             },
             (ValidationArg::Fail, ValidationArg::Other) | 
             (ValidationArg::Other, ValidationArg::None) => {
-                Err("Argument error: invalid value".red().to_string())
+                Err("Argument error: invalid value".into())
             },
             (ValidationArg::GenerateFile, ValidationArg::None) => {
                 Utils::show_message_success_with_file(test_errors.to_vec());
